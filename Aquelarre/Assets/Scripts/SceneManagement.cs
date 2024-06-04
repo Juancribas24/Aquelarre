0a7d099fda0f593d06ae90b230407f089de2e256
Daniel <carlos_dan.gomez@uao.edu.co>
2024-06-03 19:03:59 -0500

    Menu Principal (2)
    SceneManager y funcionamiento basico del menu principal

---
+++ Aquelarre/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Load a scene by name
+    public void LoadScene(string sceneName)
+    {
+        if (SceneExists(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError($"Scene '{sceneName}' does not exist. Please check the scene name.");
+        }
+    }
+
+    // Reload the current scene
+    public void ReloadCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+
+    // Load the next scene in the build settings
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("No next scene found. Make sure you have more scenes added to the build settings.");
+        }
+    }
+
+    // Quit the application
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor, so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    // Check if the scene exists in build settings
+    private bool SceneExists(string sceneName)
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            string sceneFileName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+
+            if (sceneFileName == sceneName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
